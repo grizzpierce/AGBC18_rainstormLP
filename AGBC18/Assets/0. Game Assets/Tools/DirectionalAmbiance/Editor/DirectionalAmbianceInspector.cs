f8c809b08b61815dda8af57a868a97960d4a519f
Jakey Mumfie <jakeymumfie@gmail.com>
2018-11-29 10:06:23 -0600

    Organized scene and started on tool

---
+++ AGBC18/Assets/0. Game Assets/Tools/DirectionalAmbiance/Editor/DirectionalAmbianceInspector.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(DirectionalAmbiance))]
+public class DirectionalAmbianceInspector : Editor {
+
+	private DirectionalAmbiance _myTarget;
+
+	private SerializedObject _mySerializedObject;
+	private SerializedProperty _serializedEventData;
+
+	private void OnEnable () {
+		_myTarget = (DirectionalAmbiance)target;
+
+		_mySerializedObject = new SerializedObject( _myTarget );
+		_serializedEventData = _mySerializedObject.FindProperty("_fmodEvent");
+	}
+
+	private void OnDisable () {
+
+	}
+
+	private void OnDestroy () {
+
+	}
+
+	public override void OnInspectorGUI () {
+		DrawEventDataGUI();
+		DrawDirectionalDataGUI();
+		DrawDecibalDataGUI();
+
+		EditorUtility.SetDirty(_myTarget);
+		
+	}
+
+    
+	private void DrawEventDataGUI()
+    {
+		EditorGUILayout.PropertyField (_serializedEventData);
+		_mySerializedObject.ApplyModifiedProperties();
+    }
+
+    private void DrawDirectionalDataGUI()
+    {
+        EditorGUILayout.LabelField("Directional Data", EditorStyles.boldLabel);
+		EditorGUILayout.BeginVertical("box");
+
+		_myTarget._orientation = EditorGUILayout.Slider("Orientation", _myTarget._orientation, -180, 180);
+		_myTarget._spread = EditorGUILayout.Slider("Spread", _myTarget._spread, 0, 90);
+		_myTarget._width = EditorGUILayout.Slider("Width", _myTarget._width, 0, 90);
+
+		EditorGUILayout.EndVertical();
+    }
+
+	private void DrawDecibalDataGUI()
+    {
+		EditorGUILayout.LabelField("Levels", EditorStyles.boldLabel);
+		EditorGUILayout.BeginVertical("box");
+
+		EditorGUILayout.LabelField("High dB: ", _myTarget._maxVolume.ToString());
+        EditorGUILayout.LabelField("Low dB: ", _myTarget._minVolume.ToString());
+
+		EditorGUILayout.MinMaxSlider(ref _myTarget._minVolume, ref _myTarget._maxVolume, -75, 0);
+
+		EditorGUILayout.EndVertical();
+    }
+
+}
---
+++ AGBC18/Assets/0. Game Assets/Tools/DirectionalAmbiance/Scripts/DirectionalAmbiance.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalAmbiance : MonoBehaviour {
+
+	[FMODUnity.EventRef]
+	public string _fmodEvent;
+	
+	[Range(-180,180)]
+	public float _orientation = 0;
+	[Range(0,90)]
+	public float _spread = 0;
+	[Range(0,90)]
+	public float _width = 0;
+
+	[Range(-75,0)]
+	public float _maxVolume = 0;
+	[Range(-75,0)]
+	public float _minVolume = 0;
+
+	FMOD.Studio.EventInstance _thisEvent;
+
+	Camera _mainCamera;
+
+	void Awake () {
+		_mainCamera = gameObject.GetComponentInParent<Camera>();
+	}
+
+	void Start () {
+		_thisEvent = FMODUnity.RuntimeManager.CreateInstance(_fmodEvent);
+		_thisEvent.start();
+	}
+	
+	void Update () {
+		
+	}
+
+	Vector3 from = Vector3.zero;
+	Vector3 direction = Vector3.forward;
+	void OnDrawGizmos () {
+		Color oldColor = Gizmos.color;
+		Matrix4x4 oldMatrix = Gizmos.matrix;
+		Gizmos.matrix = transform.localToWorldMatrix;
+
+		OrientationVectorGizmo();
+
+		Gizmos.matrix = oldMatrix;
+		Gizmos.color = oldColor;
+	}
+
+	void OnDrawGizmosSelected () {
+		Color oldColor = Gizmos.color;
+		Matrix4x4 oldMatrix = Gizmos.matrix;
+		Gizmos.matrix = transform.localToWorldMatrix;
+
+		OrientationVectorGizmo();
+
+		Gizmos.matrix = oldMatrix;
+		Gizmos.color = oldColor;
+	}
+
+	private void OrientationVectorGizmo() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(from, direction);
+	}
+}
